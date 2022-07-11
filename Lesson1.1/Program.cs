7c1ec3bb496dcfbc973a34bfb6f540cf6770e0fa
Prokv <prokv_82@mail.ru>
2022-07-11 12:14:06 +0400

    Program.cs

---
+++ Lesson1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string? name = Console.ReadLine();       // вводим имя
+            Console.WriteLine($"Привет {name}");    // выводим имя на консоль
+        }
+    }
+}
