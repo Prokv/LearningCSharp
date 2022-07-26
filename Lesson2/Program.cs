63537005240852d8d5d27017a31f77563a0cd3fa
Prokv <prokv_82@mail.ru>
2022-07-26 13:26:18 +0400

    Класс TicTacToeGame.cs и проект Lesson2/Program.cs
    Изменения для игры крестики-нолики.

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Игра крестики-нолики."+ $"{Environment.NewLine}Хотите сыграть в игру?" +
+            Console.WriteLine("Игра крестики-нолики."+ $"{Environment.NewLine}Хотите сыграть в игру?" +
                 $"{Environment.NewLine}[Нажмите Y если согласны или Любую клавишу если нет]");
             ConsoleKeyInfo input = Console.ReadKey();
             Console.WriteLine();
-            if (input.Key == ConsoleKey.Y) 
+            if (input.Key == ConsoleKey.Y)
             {
                 string[] Field = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
                 string name;
@@ -20,26 +20,17 @@
                     if (i % 2 > 0)
                     {
                         name = "X";
-                        i= TicTacToeGame.StepCheck(name, Field, i);
                     }
                     else {
                         name = "O";
-                        i= TicTacToeGame.StepCheck(name, Field, i);
                     }
+                    TicTacToeGame.StepCheck(name, Field, ref i);
                     TicTacToeGame.Grid(Field);
-                    if (i > 4)
-                    {
-                        i = TicTacToeGame.WinCheck(name, Field, i);
-                    }
-                    if (i == Field.Length+1)
-                    {
-                        Console.WriteLine("Ничья!Победила дружба.");
-                    }
+                    if (i > 4) TicTacToeGame.WinCheck(name, Field, ref i);
+                    if (i == Field.Length+1) Console.WriteLine("Ничья!Победила дружба.\a");
                 }
             }
-            else {
-                Console.WriteLine("Отказ от игры!");  
-                  }
+            else Console.WriteLine("Отказ от игры!");
         }
     }
 }
--- Lesson2/TicTacToeGame.cs
+++ Lesson2/TicTacToeGame.cs
@@ -11,23 +11,19 @@ namespace Lesson2
         /// <summary>
         /// Проверка наличия выигрышной комбинации.
         /// </summary>
-        /// <param name="name">Имя игрока.</param>
-        /// <param name="Field">Массив номеров клеток игрового поля.</param>
-        /// <param name="i">Порядковый номер хода.</param>
-        public static int WinCheck(string name, string[] Field, int i)
+        public static void WinCheck(string name, string[] Field, ref int i)
         {
-            int[,] WinArray = new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
+            int[,] WinArray = new int[,] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
 
+            
             for (int r = 0; r < 8; r++)
             {
                 if (Field[WinArray[r, 0]] == Field[WinArray[r, 1]] & Field[WinArray[r, 1]] == Field[WinArray[r, 2]])
                 {
-                    Console.WriteLine("Победа игрока " + name + ". Игра окончена");
+                    Console.WriteLine($"Победа игрока: {name}. Игра окончена.\a");
                     i = Field.Length + 2;
-                    return i;
                 }
             }
-            return i;
         }
         /// <summary>
         /// Раскраска поля в зависимости от хода игрока.
@@ -62,45 +58,44 @@ namespace Lesson2
                 Cell_Color(Field, min, max);
                 Console.WriteLine();
                 min = max;
-                max = max + 3;
+                max +=3;
             }
             Console.WriteLine("-------");
         }
         /// <summary>
         /// Определяет очередность хода и правильность выбора поля для хода.
         /// </summary>
-        public static int StepCheck(string name, string[] Field, int i)
+        public static void StepCheck(string name, string[] Field, ref int i)
         {
             int step;
-            Console.Write("Ход игрока " + name + ":");
+            Console.Write($"Ход игрока {name}:");
             if (int.TryParse(Console.ReadLine(), out int numValue))
             {
                 step = numValue - 1;
                 if (numValue > 0 & numValue <= Field.Length)
                 {
-                    if (Field[step] != "X" & Field[step] != "O")
+                    if (Field[step] != "X" && Field[step] != "O")
                     {
                         Console.Clear();
                         Field[step] = name;
                         i++;
-                        return i;
                     }
                     else
                     {
-                        Console.WriteLine("Ход игрока " + name + ": Поле занято, ход невозможен!");
-                        return i;
+                        Console.Clear();
+                        Console.WriteLine($"Ход игрока {name}: {numValue}. Поле занято, ход невозможен! Повторите попытку.");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Ход игрока " + name + ": Ход невозможен");
-                    return i;
+                    Console.Clear();
+                    Console.WriteLine($"Ход игрока {name}: {numValue}. Ход невозможен. Повторите попытку.");
                 }
             }
             else
             {
-                Console.WriteLine("Некорректный ввод");
-                return i;
+                Console.Clear();
+                Console.WriteLine($"Ход игрока {name}.Некорректный ввод. Введите цифровое значение! Повторите попытку.");
             }
         }
     }
