7168ccc80098a3c78c140423713ca150b70ea852
Prokv <prokv_82@mail.ru>
2022-08-05 14:36:49 +0400

    Lesson7.2 Задача по написанию класса типа List

---
+++ Lesson7.2/MyList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7._2
+{
+    internal class MyList
+    {
+        public string[] List;
+        public int Count;
+
+        public MyList (string[] list)
+        {
+            this.List = list;
+        }
+    }
+}
---
+++ Lesson7.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
