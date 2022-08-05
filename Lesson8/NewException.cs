d57a464499b970cd664eb24ce872aef91ac8dbfb
Prokv <prokv_82@mail.ru>
2022-08-05 14:36:12 +0400

    Lesson8 Написание блока с конструкцией try..catch

---
+++ Lesson8/NewException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    internal class NewException : Exception
+    {   
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="message">Текст сообщения ошибки</param>
+        public NewException(string message) : base(message) { }
+    }
+}
---
+++ Lesson8/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст:");
+            try
+            {
+                string line = Console.ReadLine();
+                if (line == "")
+                {
+                    throw new NewException("Ввод пустой строки недопустим");
+                }
+            }
+            catch (NewException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally { Console.WriteLine("Конец программы"); }
+
+        }
+    }
+}
