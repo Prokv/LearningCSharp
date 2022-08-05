8dd68b45a54854c58e146e89c5e45338e221a196
Prokv <prokv_82@mail.ru>
2022-08-06 01:17:55 +0400

    Дз по созданию класса типа List<T>

--- Lesson7.2/MyList.cs
+++ Lesson7.2/MyList.cs
@@ -6,14 +6,70 @@ using System.Threading.Tasks;
 
 namespace Lesson7._2
 {
-    internal class MyList
+    internal class MyList <T>
     {
-        public string[] List;
-        public int Count;
+        /// <summary>
+        /// Начальный список
+        /// </summary>
+        public T[] List=new T[0];
 
-        public MyList (string[] list)
+        private int count=0;
+        /// <summary>
+        /// Количество содержащихся элементов в массиве
+        /// </summary>
+        public int Count { get { return count; } }
+        public int j = 0;
+
+        /// <summary>
+        /// Добавление элемента в список
+        /// </summary>
+        /// <param name="List">Имя списка</param>
+        public void Add (T List)
         {
-            this.List = list;
+            count++;
+            Array.Resize(ref this.List, count);
+            this.List[j] = List;
+            j++;
+        }
+        /// <summary>
+        /// Очистка списка значений, сброс всех счетчиков
+        /// </summary>
+        public void Clear ()
+        {
+            count = 0;
+            j = 0;
+            List = new T[0];
+        }
+
+        /// <summary>
+        /// Индексатор для возврата значения по индексу
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T this[int index]
+        {
+            get => List[index];
+            set => List[index] = value;
+        }
+        /// <summary>
+        /// Удаление элемента массива по его значению
+        /// </summary>
+        /// <param name="name">Значение элемента массива, которое требуется удалить</param>
+        public void Del (T name)
+        {
+            int identic=0;
+            j = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (Equals(List[i], name)) { identic++; }
+                else
+                {
+                    List[j]=List[i];
+                    j++;
+                }
+            }
+            count=count-identic;
+            Array.Resize(ref this.List, count);
         }
     }
 }
--- Lesson7.2/Program.cs
+++ Lesson7.2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
+            MyList<int> list1 = new MyList<int>();
+            int n = 6;
+            for (int i = 0; i < n; i++)
+            {
+                list1.Add(i);
+                Console.WriteLine(list1[i]);
+            }
+            list1[3] = 4;
+            Console.WriteLine("------------");
+            Console.WriteLine(list1[3]); //получаем элемент по индексу
+            Console.WriteLine("------------");
+            Console.WriteLine(list1.Count); //получаем количество элементов в массиве
+
+            list1.Del(4);
+            Console.WriteLine("------------");
+            Console.WriteLine(list1[4]);
+            Console.WriteLine("------------");
+            Console.WriteLine(list1.Count);
+            Console.WriteLine("------------");
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine(list1[i]);
+            }
         }
     }
 }
